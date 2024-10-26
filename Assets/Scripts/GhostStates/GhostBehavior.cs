1bcfeaa23a557232feb6f5328236c156b02e442f
ocai0 <caioalvesfernandes@gmail.com>
2024-10-26 10:33:19 -0300

    Inplemented GhostBehavior

--- Assets/Scripts/GhostStates/GhostBehavior.cs
+++ Assets/Scripts/GhostStates/GhostBehavior.cs
@@ -1,4 +1,23 @@
 using UnityEngine;
 
-public class GhostBehaviour : MonoBehavior {
+[RequireComponent(typeof(Ghost))]
+public abstract class GhostBehaviour : MonoBehavior {
+    public Ghost ghost { get; private set; }
+    public float duration;
+    private void Awake() {
+        this.ghost = GetComponent<Ghost>();
+        this.enabled = false;
+    }
+    public void Enable() {
+        EnableDuration(this.duration);
+    }
+    public virtual void Enable(float duration) {
+        this.enabled = true;
+        CancelInvoke();
+        Invoke(nameof(Disable), duration);
+    }
+    public virtual void Disable() {
+        this.enabled = false;
+        CancelInvoke();
+    }
 }
