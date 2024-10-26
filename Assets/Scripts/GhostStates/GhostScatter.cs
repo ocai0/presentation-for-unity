a023414e08576e64b1e7fa01f9d0e3021dc29af1
ocai0 <caioalvesfernandes@gmail.com>
2024-10-26 14:40:05 -0300

    Added Ghost Scatter Logic

--- Assets/Scripts/GhostStates/GhostScatter.cs
+++ Assets/Scripts/GhostStates/GhostScatter.cs
@@ -1,4 +1,17 @@
 using UnityEngine;
 
 public class GhostScatter : GhostBehavior {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(!this.enabled || this.ghost.frightned.enabled) return;
+        Node node = other.GetComponent<Node>();
+        if(node == null) return;
+        int index = Random.Range(0, node.avaiableDirections.Count - 1);
+
+        if(node.avaiableDirections[index] == -this.ghost.movement.direction && node.avaiableDirections.Count > 1) {
+            index++;
+            if(index > node.avaiableDirections.Count) index = 0;
+        }
+        Debug.Log(index);
+        this.ghost.movement.SetDirection(node.avaiableDirections[index]);
+    }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class Node : MonoBehaviour {
     public LayerMask obstacleLayer;
-    public List<Vector2> avaiableDirecitons { get; private set; }
-    void Start() {
-        this.avaiableDirecitons = new List<Vector2>();
+    public List<Vector2> avaiableDirections { get; private set; }
+    private void Start() {
+        this.avaiableDirections = new List<Vector2>();
 
         CheckAvaiableD2rections(Vector2.up);
         CheckAvaiableD2rections(Vector2.down);
@@ -13,8 +13,8 @@ public class Node : MonoBehaviour {
         CheckAvaiableD2rections(Vector2.right);
     }
 
-    void CheckAvaiableD2rections(Vector2 direction) {
-        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector.one * .5f, .0f, direction, 1.0f, this.obstacleLayer);
-        if(hit.collider == null) this.avaiableDirecitons.Add(direction);
+    private void CheckAvaiableD2rections(Vector2 direction) {
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * .5f, .0f, direction, 1.0f, this.obstacleLayer);
+        if(hit.collider == null) this.avaiableDirections.Add(direction);
     }
 }
