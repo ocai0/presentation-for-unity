eec6bc71ddcc14e7e0b2ec79989bc15cf2a51c2b
ocai0 <caioalvesfernandes@gmail.com>
2024-10-29 23:05:41 -0300

    Fixed typo

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -7,13 +7,13 @@ public class Node : MonoBehaviour {
     private void Start() {
         this.avaiableDirections = new List<Vector2>();
 
-        CheckAvaiableD2rections(Vector2.up);
-        CheckAvaiableD2rections(Vector2.down);
-        CheckAvaiableD2rections(Vector2.left);
-        CheckAvaiableD2rections(Vector2.right);
+        CheckAvaiableDirections(Vector2.up);
+        CheckAvaiableDirections(Vector2.down);
+        CheckAvaiableDirections(Vector2.left);
+        CheckAvaiableDirections(Vector2.right);
     }
 
-    private void CheckAvaiableD2rections(Vector2 direction) {
+    private void CheckAvaiableDirections(Vector2 direction) {
         RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * .5f, .0f, direction, 1.0f, this.obstacleLayer);
         if(hit.collider == null) this.avaiableDirections.Add(direction);
     }
